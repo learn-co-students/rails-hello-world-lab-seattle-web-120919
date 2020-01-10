1c47786168f130547358462b4a98ce11ce80bb16
github username <github email address>
2020-01-10 15:14:30 -0800

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,7 @@
 class StaticController < ApplicationController
     def hello_world
         render '/hello_world'
+        # explicitly force to print 'hello_world erb' file IF 
+        # not found in views > static > hello_world 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'hello_world', to: 'static#hello_world'
-
+  # render to the route '/hello_world', when calling #hello_world method in controller 'static'
+  # Rails auto-looks inside the controller's corresponding views folder: views > 'static' 
 end
